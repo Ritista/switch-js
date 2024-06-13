8ec3b2517afa241ffc3998a9cf149295081d1223
Ritista Shrestha <ritista99@gmail.com>
2024-06-13 14:34:28 +0545

    first commit

---
+++ script1.js
@@ -0,0 +1,22 @@
+console.log("day 11");
+//condtion
+//if, else, else if
+//switch
+// for loop
+
+let a="saturday";
+switch(a){
+  case"monday":
+    console.log("today is monday");
+    break;
+  case"sunday", "saturday":
+    console.log("today is holiday");
+    break;
+  default:
+    console.log("none of the above");
+}
+
+//for loop
+for(i = 10; i >= 0; i--){
+    console.log(i);
+}
